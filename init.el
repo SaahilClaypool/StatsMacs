4fb534fa616cc1d7c97dc203af17c0146c5f87bf
saahilclaypool <saahil@claypools.org>
2017-03-05 12:24:01 -0500

    fixed Rmd mode

--- init.el
+++ init.el
@@ -26,6 +26,18 @@
   )
   
 
+(use-package polymode
+  :ensure t
+  :config
+  (require 'poly-R)
+  ;;; MARKDOWN
+  (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+
+;;; R modes
+  (add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
+  (require 'poly-markdown))
 (use-package ess
   :ensure t)
 
@@ -42,7 +54,7 @@
   )
 (use-package counsel
   :config
-  (global-set-key (kbd "M-x") 'counsel-M-x)p
+  (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-c C-y") 'counsel-yank-pop )
   (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
@@ -90,3 +102,17 @@
 ;; no bars
 (tool-bar-mode -1)
 (setq inhibit-startup-message t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (polymode web-mode w3m use-package tabbar solarized-theme neotree monokai-theme memoize markdown-preview-mode markdown-mode+ javadoc-lookup google-this flycheck evil-surround evil-org evil-mc evil-magit ess elpy editorconfig counsel-projectile base16-theme avy atom-one-dark-theme ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
